2d2caed17b8f4c9513f6e8dccea2e18b03bd2c07
Muhammad Aditya Ridharrahman <adityaridharrahman@gmail.com>
2024-11-18 10:58:13 +0700

    add test to test suite

--- Test Suites/WEB/Saucedemo Test Suite.ts
+++ Test Suites/WEB/Saucedemo Test Suite.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>9df2b005-b940-40e3-8ea3-5ec07befb3bb</testSuiteGuid>
+   <testCaseLink>
+      <guid>4320a957-7164-4813-b3b4-3b1cb63eae65</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/Saucedemo/Login with standard user</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>

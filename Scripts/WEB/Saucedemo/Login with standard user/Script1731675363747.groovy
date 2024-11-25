2c7072390fcb550c29d59315d270991260bb3109
adityaridha <aditya.ridharrahman@wartek.belajar.id>
2024-11-25 16:50:56 +0700

    add test object

--- Scripts/WEB/Saucedemo/Login with standard user/Script1731675363747.groovy
+++ Scripts/WEB/Saucedemo/Login with standard user/Script1731675363747.groovy
@@ -19,19 +19,39 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('https://www.saucedemo.com/')
 
-CustomKeywords.'sauce.LoginPage.login'(findTestObject('WEB/Saucedemo/login/input_Swag Labs_user-name'), findTestObject('WEB/Saucedemo/login/input_Swag Labs_password'), username, password)
+WebUI.verifyElementVisible(findTestObject('WEB/Saucedemo/login/Page_Swag Labs/div_Accepted usernames arestandard_userlocked_out_userproblem_userperformance_glitch_usererror_uservisual_userPassword for all userssecret_sauce'))
+
+WebUI.verifyElementVisible(findTestObject('WEB/Saucedemo/login/Page_Swag Labs/div_Swag Labs'))
+
+WebUI.verifyElementVisible(findTestObject('WEB/Saucedemo/login/Page_Swag Labs/div_Swag Labs_login_wrapper-inner'))
+
+WebUI.verifyElementVisible(findTestObject('WEB/Saucedemo/login/Page_Swag Labs/h4_Password for all users'))
+
+CustomKeywords.'sauce.LoginPage.login'(findTestObject('WEB/Saucedemo/login/input_Swag Labs_user-name'), findTestObject('WEB/Saucedemo/login/input_Swag Labs_password'), 
+    username, password)
 
 //CustomKeywords.'sauce.LoginPage.inputUsername'(findTestObject('WEB/Saucedemo/login/input_Swag Labs_user-name'), 'standard_user')
 //
 //WebUI.setText(findTestObject('WEB/Saucedemo/login/input_Swag Labs_password'), password)
-
 WebUI.click(findTestObject('WEB/Saucedemo/login/input_Swag Labs_login-button'))
 
 WebUI.verifyElementVisible(findTestObject('WEB/Saucedemo/dashboard/button_Add to cart'))
 
-WebUI.verifyElementPresent(findTestObject('WEB/Saucedemo/dashboard/span_Products'), 0)
+WebUI.verifyElementVisible(findTestObject('WEB/Saucedemo/dashboard/a_Swag Labs_shopping_cart_link'))
 
-CustomKeywords.'sauce.LoginPage.refreshBrowser'()
+WebUI.verifyElementVisible(findTestObject('WEB/Saucedemo/dashboard/button_Open Menu'))
 
+WebUI.verifyElementVisible(findTestObject('WEB/Saucedemo/dashboard/div_29.99'))
 
+WebUI.verifyElementVisible(findTestObject('WEB/Saucedemo/dashboard/span_Name (A to Z)Name (A to Z)Name (Z to A)Price (low to high)Price (high to low)'))
+
+WebUI.verifyElementVisible(findTestObject('WEB/Saucedemo/dashboard/span_Products'))
+
+WebUI.verifyElementVisible(findTestObject('WEB/Saucedemo/dashboard/Page_Swag Labs/div_carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection'))
+
+WebUI.verifyElementVisible(findTestObject('WEB/Saucedemo/dashboard/Page_Swag Labs/div_15.99'))
+
+WebUI.verifyElementVisible(findTestObject('WEB/Saucedemo/dashboard/Page_Swag Labs/div_49.99'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('WEB/Saucedemo/dashboard/Page_Swag Labs/div_carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection'))
 
